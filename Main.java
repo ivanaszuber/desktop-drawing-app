6d77e3d36a65cc4633c1ea1997b5aebe9248e9fd
ivanaszuber <ivanas.zuber@gmail.com>
2014-08-04 21:38:53 -0800

    added Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ *   JAVA DRAWING APP
+ *   Ivana Zuber
+ *   March 2013
+ */
+
+public class Main
+{
+    public static PaintApplication paint;
+
+    /**
+     * Application starting point, creates a new instance of the PaintApplication class
+     * @param args String arguments
+     */
+    public static void main(String args[])
+    {
+        paint = new PaintApplication();
+    }
+
+
+}
