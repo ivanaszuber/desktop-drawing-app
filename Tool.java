40c2765902d5e698aef3cfa74e2c5ac6fc2e3d9e
ivanaszuber <ivanas.zuber@gmail.com>
2014-08-09 19:32:10 -0400

    progress

---
+++ Tool.java
@@ -0,0 +1,15 @@
+/**
+ *   JAVA DRAWING APP
+ *   Ivana Zuber
+ *   March 2013
+ */
+
+//class used to create different tool instances, depending on the given type parameter
+public class Tool
+{
+    public int toolType;
+    public Tool (int type)
+    {
+       toolType = type;
+    }
+}
