e4bd23042a84010184e207ff21df192391771ebc
ivanaszuber <ivanas.zuber@gmail.com>
2014-08-18 19:32:10 -0400

    added Main

---
+++ out/production/Java drawing app/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static PaintApplication paint;
+
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        paint = new PaintApplication();
+    }
+}
